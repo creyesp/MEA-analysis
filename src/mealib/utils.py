87a6ffb288ace497a0e349c9f18c85640e6a4837
César Reyes <cesar.reyesp@gmail.com>
2018-09-30 14:47:15 -0300

    change name funtions

--- src/mealib/utils.py
+++ src/mealib/utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-def cleanDirectory(inputfolder):
+def clean_directory(inputfolder):
     """Delete all files and drectories in input directory.
 
     If the directory doesn't exist then this directory is create.
@@ -27,7 +27,7 @@ def cleanDirectory(inputfolder):
             for files in os.listdir(inputfolder):
                 os.remove(inputfolder+files)
 
-def checkDirectory(inputfolder):
+def check_directory(inputfolder):
     """Check if exist the directory, else make a new folder.
 
     If the directory doesn't exist then this directory is create.
