5e9cdddc7690f0197cec2a2080bef3d29fb05ddc
César Reyes <cesar.reyesp@gmail.com>
2018-11-20 17:27:10 -0300

    move sync script version to script folder

---
+++ src/mealib/scripts/sync_batch.py
@@ -0,0 +1,29 @@
+from mealib.utils import check_directory
+from mealib.preprocessing import Sync
+
+if __name__ == '__main__':
+
+    exp = {'MR-0181':0}
+    real_fps = 59.7607
+    
+    for kexp in exp:
+        source_folder = '../data/raw_data/'+kexp+'/'+kexp+'_analog.mcd'
+        output_folder = '../data/sync/'+kexp+'/'
+        output_folder_event = '../data/sync/'+kexp+'/event_list/'
+        mcd_channel = exp[kexp]
+        check_directory(output_folder)
+        check_directory(output_folder_event)
+
+        print('\n'+kexp)
+        sync_data = Sync(kexp, real_fps)
+        sync_data.read_mcd(source_folder)
+        sync_data.show_entities()
+        sync_data.analyzer(mcd_channel)
+
+        sync_data.create_events()
+        sync_data.add_repeated()
+
+        sync_data.save_analyzed(output_folder)
+        sync_data.save_events(output_folder)
+        sync_data.create_separated_sync(output_folder_event)
+        sync_data.close_file()
